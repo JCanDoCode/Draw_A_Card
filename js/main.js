d41015aca5bb48faecf1d3c24f47d692b49c899f
JCanDoCode <156962029+JCanDoCode@users.noreply.github.com>
2024-10-30 21:25:32 -0700

    Update 1
    Implemented Deck of Cards API for card images. Added logo. Made it possible for more than 4 cards to be drawn at once. Added a clear cards button

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@ const container = document.querySelector(".container");
 const card_container = document.querySelector(".card-container");
 const selected_card = document.querySelector(".selected-card");
 const card_place = document.querySelector(".card-place");
-const card_array = ["card1", "card2", "card3", "card4"];
 const rect = container.getBoundingClientRect();
 let active = false;
 
 const card_select = () => {
-    const card = document.querySelector(".card").className = "card:nth-of-type(1)";
+    
     selected_card.style.display = "block";
     active = true;
 }
@@ -18,25 +17,26 @@ const move_card = (e) => {
         selected_card.style.transform = "translate(" + cursorX + "px," + cursorY + "px)";
     }
 }
-const choose_card = (min, max) =>{
+async function choose_card() {
     if (active==true) {
         selected_card.style.display = "none";
         var audio = new Audio("./sfx/flip.mp3");
         audio.play();
-        const random_card = document.createElement("img");
-        const card_number = card_array[Math.round(Math.random()*(max-min)+min)];
-        if (card_number == undefined) {
-        console.log("Undefined image detected");
-        } else {
-        random_card.src = "./images/" + card_number + ".png";
-        random_card.className = "card_number";
-        document.querySelector(".card-place").appendChild(random_card);
+        try {
+            const response = await fetch("https://www.deckofcardsapi.com/api/deck/new/draw/?count=1")
+            const deck = await response.json();
+            const random_card = document.createElement("img");
+            random_card.src = deck.cards[0].image;
+            random_card.className = "drawn-card";
+            document.querySelector(".card-place").appendChild(random_card);
+            active = false;
+        } catch (error) {
+            console.log(error)
         }
     } else {
         console.log("No card selected!");
     }
-    active= false;
 }
-card_place.addEventListener("click", () => {choose_card(0, 3)}, false);
+card_place.addEventListener("click", choose_card, false);
 card_container.addEventListener('click', card_select, false);
 container.addEventListener('mousemove', move_card, false);
